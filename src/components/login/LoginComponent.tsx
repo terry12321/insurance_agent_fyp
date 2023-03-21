2ab156d5234029a1949003d9d0ea4cfe937b216c
terry12321 <tanzhenxuan76@gmail.com>
2023-03-21 17:04:25 +0800

    feat: add logo

--- src/components/login/LoginComponent.tsx
+++ src/components/login/LoginComponent.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import React, { FormEvent, useCallback, useState } from "react";
 import { useUserStore } from "src/stores/UserStore";
 import { toast, Toaster } from "react-hot-toast";
+import Image from "next/image";
 
 const LoginComponent = () => {
     const [username, setUsername] = useState("");
@@ -29,42 +30,14 @@ const LoginComponent = () => {
     );
     return (
         <>
-            <div className="w-7/12 text-cyan-100 flex flex-col justify-center items-center">
-                <span className="text-center text-7xl font-bold rounded-full py-10 px-20 border-dotted border-8 border-cyan-100 w-fit flex flex-col">
-                    <span>Welcome to</span>
-                    <span>FA-Guardix</span>
-                </span>
-            </div>
-            <form
-                onSubmit={(e) => handleClick(e)}
-                className="bg-cyan-100 text-black w-5/12"
-            >
-                <div className="flex flex-col gap-12 justify-center items-center h-full">
-                    <span className="text-4xl text-white">Login</span>
-                    <div className="flex flex-col gap-2 w-full items-center mb-24">
-                        <input
-                            className="rounded-lg p-2 bg-white w-2/4 focus:outline-none focus:ring focus:ring-cyan-500"
-                            type={"text"}
-                            placeholder="username"
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                        <input
-                            className="rounded-lg p-2 bg-white w-2/4 focus:outline-none focus:ring focus:ring-cyan-500"
-                            type={"password"}
-                            placeholder="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <button
-                            type="submit"
-                            className="bg-white rounded-full px-4 py-1 mt-2 text-white-500 hover:bg-cyan-200 hover:text-white"
-                        >
-                            login
-                        </button>
-                    </div>
+            <div className="w-full">
+                <div className="flex bg-black w-full h-20">
+                    <span className="flex items-center px-4">
+                    <Image className="" alt={"FA-Guardix"} width={200} height={200} src={"/images/logo.svg"} />
+
+                    </span>
                 </div>
-            </form>
+            </div>
             <Toaster
                 position="bottom-right"
                 reverseOrder={false}
