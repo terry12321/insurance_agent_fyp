bb24d7e3df37ad0f179161bfe55e11c6191f789a
terry12321 <tanzhenxuan76@gmail.com>
2023-03-31 15:16:57 +0800

    refactor: update remove forum code for now

--- src/pages/forum.tsx
+++ src/pages/forum.tsx
@@ -1,6 +1,5 @@
 export default function Forum() {
     return (
-        <div className="bg-white text-gray-100 h-5/6 w-5/6 grid grid-cols-3 rounded-[24px] gap-6 p-10 overflow-auto">
-        </div>
+        <div className="bg-white text-gray-100 h-5/6 w-5/6 grid grid-cols-3 rounded-[24px] gap-6 p-10 overflow-auto"></div>
     );
 }
