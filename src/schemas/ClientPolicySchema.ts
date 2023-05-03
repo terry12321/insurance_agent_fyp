c1fa2d19f5162ab4059f70316c90169bc9a8f905
terry12321 <tanzhenxuan76@gmail.com>
2023-05-03 18:01:58 +0800

    refactor: update policy schema for yup validation

--- src/schemas/ClientPolicySchema.ts
+++ src/schemas/ClientPolicySchema.ts
@@ -2,6 +2,12 @@ import * as yup from "yup";
 
 export const ClientPolicySchema = yup.object({
     name: yup.string().required("Name is required"),
-    premium: yup.string().matches(/^[0-9]*$/g,"Incorrect premium format").required("Premium is required"),
-    coverage: yup.string().matches(/^[0-9]*$/g,"Incorrect coverage format").required("Coverage is required"),
+    premium: yup
+        .string()
+        .matches(/^[0-9]*$/g, "Incorrect premium format")
+        .required("Premium is required"),
+    coverage: yup
+        .string()
+        .matches(/^[0-9]*$/g, "Incorrect coverage format")
+        .required("Coverage is required"),
 });
